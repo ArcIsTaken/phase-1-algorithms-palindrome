81d7d51d2cc7d99df1a6a0f0d57436d32fcd4191
Riley <riley.freier@gmail.com>
2023-08-29 14:32:55 -0600

    this was nice

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+
+ let stringOrig = word;
+
+ // takes the word and reverses it though an IIFE
+ let stringReverse = (function () {
+  let splitWord = word.split("");
+  let reverseWord = splitWord.reverse();
+  let wordReverse = reverseWord.join("");
+  return (wordReverse);
+})();
+ 
+
+//Compares the 2 variables
+ if (`${stringOrig}` == `${stringReverse}`) {
+    return (true);
+ } else {
+    return (false);
+ };
+};
 
 /* 
   Add your pseudocode here
-*/
+  function (imput string)>
+    save string to temporary variable
+    copy the string into a second variable 
+    reverse copy
+    check both variables to see if they ==
+    returns "true" is == or "false" is !=
+    return
+*/ 
+
 
 /*
   Add written explanation of your solution here
+  the function "isPalindrome" takes a word as input
+  then saves the word to 2 variables.
+  one variable reversed the string though an IIFE.
+  then both variables are compared for equality in an if else statement,
+  then its returns true if they are equal.
 */
 
 // You can run `node index.js` to view these console logs
